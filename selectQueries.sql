c9f560a7524c724581203779c7ffa891d4f36a01
matbotW <mathewmcdade@gmail.com>
2020-02-23 16:31:20 -0800

    small corrections

--- selectQueries.sql
+++ selectQueries.sql
@@ -4,29 +4,45 @@
 SELECT *
 FROM user;
 
+SELECT id FROM user;
+
 # Select all fields for user by email.
 SELECT *
 FROM user
 WHERE email = ?;
 
 # Select fields for all admin users.
-SELECT id, type, email, createdon, createdby
+SELECT id, type, email, createdOn, createdBy
 FROM user
          JOIN admin a ON user.id = a.user_id;
 
 # Select fields for admin user by id.
-SELECT id, type, email, createdon, createdby
+SELECT id, type, email, createdOn, createdBy
 FROM user
          JOIN admin a ON user.id = a.user_id
 WHERE a.user_id = ?;
 
 # Select fields for all manager users.
-SELECT id, type, email, createdOn, createdBy, firstName, lastName, signatureURL
+SELECT id,
+       type,
+       email,
+       createdOn,
+       createdBy,
+       firstName,
+       lastName,
+       signatureURL
 FROM user
          JOIN manager m ON user.id = m.user_id;
 
 # Select fields for manager user by id.
-SELECT id, type, email, createdOn, createdBy, firstName, lastName, signatureURL
+SELECT id,
+       type,
+       email,
+       createdOn,
+       createdBy,
+       firstName,
+       lastName,
+       signatureURL
 FROM user
          JOIN manager m ON user.id = m.user_id
 WHERE m.user_id = ?;
@@ -71,5 +87,24 @@ WHERE (award.createdOn BETWEEN ? AND ?)
   AND (? IS NULL OR award.type LIKE ?)
   AND (? IS NULL OR award.recipientName LIKE ?)
   AND (? IS NULL OR award.recipientEmail LIKE ?)
+  AND (? IS NULL OR r.id = ?)
+  AND (? IS NULL OR m.user_id = ?);
+
+# Deployed API parameter query.
+SELECT a.id,
+       r.name,
+       a.type,
+       a.recipientName,
+       a.recipientEmail,
+       a.createdOn,
+       m.firstName,
+       m.lastName
+FROM award a
+         JOIN region r ON a.region_id = r.id
+         JOIN manager m ON a.createdBy = m.user_id
+WHERE (a.createdOn BETWEEN ? AND ?)
+  AND (? IS NULL OR a.type LIKE ?)
+  AND (? IS NULL OR a.recipientName LIKE ?)
+  AND (? IS NULL OR a.recipientEmail LIKE ?)
   AND (? IS NULL OR r.id = ?)
   AND (? IS NULL OR m.user_id = ?);
